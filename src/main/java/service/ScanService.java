0af4b35fc2fbf299f8de4bc768c9271cabfe33a0
Hakan Sener <h-akan@hotmail.fr>
2016-12-08 16:35:02 +0100

    update manga slug and name

--- src/main/java/service/ScanService.java
+++ src/main/java/service/ScanService.java
@@ -2,6 +2,7 @@ package service;
 
 import dto.MangaDTO;
 import dto.ScanDTO;
+import util.SlugUtil;
 
 import java.io.IOException;
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 public class ScanService extends AbstractService {
 
     public List<ScanDTO> getScanDTOList(String manga) throws IOException {
-        MangaDTO mangaDTO = new MangaDTO(manga, "");
+        MangaDTO mangaDTO = new MangaDTO(SlugUtil.slugToLirescanSlug(manga), SlugUtil.slugToName(manga));
         return scanDAO.getScanDtoList(mangaDTO);
     }
 }
