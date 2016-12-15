963eb6c2b22db4133ffed893c92b640d87a0823e
Hakan Sener <h-akan@hotmail.fr>
2016-12-15 09:40:13 +0100

    add unit tests constants

--- src/test/java/dao/scanDAOTest.java
+++ src/test/java/dao/scanDAOTest.java
@@ -1,9 +1,8 @@
 package dao;
 
 import dto.MangaDTO;
-import dto.PageDTO;
-import dto.ScanDTO;
 import org.junit.Test;
+import util.ConstantUtil;
 
 import java.io.IOException;
 import java.util.List;
@@ -13,15 +12,9 @@ import static org.junit.Assert.*;
 public class scanDAOTest {
 
     private ScanDAO scanDAO;
-    private MangaDTO mangaDTO;
-    private ScanDTO scanDTO;
-    private PageDTO pageDTO;
 
     public scanDAOTest() {
         scanDAO = new LirescanDAO();
-        mangaDTO = new MangaDTO("one-piece", "One Piece");
-        scanDTO = new ScanDTO("849");
-        pageDTO = new PageDTO("1");
     }
 
     @Test
@@ -29,36 +22,36 @@ public class scanDAOTest {
         try {
             List<MangaDTO> mangaDTOList = scanDAO.getMangaDtoList();
             assertTrue("" + mangaDTOList.size(), true);
-            assertTrue("The manga list must contains <~" + mangaDTO + "~>", mangaDTOList.contains(mangaDTO));
+            assertTrue("The manga list must contains <~" + ConstantUtil.MANGA_DTO + "~>", mangaDTOList.contains(ConstantUtil.MANGA_DTO));
         } catch (IOException e) {
-            fail("The website '" + LirescanDAO.LIRE_SCAN_URL + "' is probably done.");
+            fail("The website '" + LirescanDAO.LIRE_SCAN_URL + "' is probably down.");
         }
     }
 
     @Test
     public void getScanDtoList() {
         try {
-            scanDAO.getScanDtoList(mangaDTO);
+            scanDAO.getScanDtoList(ConstantUtil.MANGA_DTO);
         } catch (IOException e) {
-            fail("The scan for <~" + mangaDTO + "~> is unreachable.");
+            fail("The scan for <~" + ConstantUtil.MANGA_DTO + "~> is unreachable.");
         }
     }
 
     @Test
     public void getPageDtoList() {
         try {
-            scanDAO.getPageDtoList(mangaDTO, scanDTO);
+            scanDAO.getPageDtoList(ConstantUtil.MANGA_DTO, ConstantUtil.SCAN_DTO);
         } catch (IOException e) {
-            fail("The page for <~" + scanDTO + "~> in <~" + mangaDTO + "~> is unreachable.");
+            fail("The page for <~" + ConstantUtil.SCAN_DTO + "~> in <~" + ConstantUtil.MANGA_DTO + "~> is unreachable.");
         }
     }
 
     @Test
     public void getImageDto() {
         try {
-            scanDAO.getImageDto(mangaDTO, scanDTO, pageDTO);
+            scanDAO.getImageDto(ConstantUtil.MANGA_DTO, ConstantUtil.SCAN_DTO, ConstantUtil.PAGE_DTO);
         } catch (IOException e) {
-            fail("The image for <~" + pageDTO + "~> in <~" + scanDTO + "~> in <~" + mangaDTO + "~> is unreachable.");
+            fail("The image for <~" + ConstantUtil.PAGE_DTO + "~> in <~" + ConstantUtil.SCAN_DTO + "~> in <~" + ConstantUtil.MANGA_DTO + "~> is unreachable.");
         }
     }
 }
