41ed0dd71118ad3fe8dc563f6bbe4a1767c42e2b
Hakan Sener <h-akan@hotmail.fr>
2017-05-13 22:54:28 +0200

    ci: add integration environment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]
 then
-  echo "Starting deployment"
-  curl -p --insecure "ftp://$DEPLOY_HOST/$DEPLOY_REMOTEDIR/" --user "$DEPLOY_USERNAME:$DEPLOY_PASSWORD" -T "$LOCAL_WAR" --ftp-create-dirs
+  echo "Starting deployment on production"
+  curl -p --insecure "ftp://$DEPLOY_HOST/$DEPLOY_REMOTEDIR/" --user "$DEPLOY_USERNAME:$DEPLOY_PASSWORD" -T "target/shonen-touch-api.war" --ftp-create-dirs
+elif [ "$TRAVIS_BRANCH" == "develop" ]
+then
+  echo "Starting deployment on integration"
+  mv target/shonen-touch-api.war target/shonen-touch-api-dev.war
+  curl -p --insecure "ftp://$DEPLOY_HOST/$DEPLOY_REMOTEDIR/" --user "$DEPLOY_USERNAME:$DEPLOY_PASSWORD" -T "target/shonen-touch-api-dev.war" --ftp-create-dirs
 else
-  echo "The work is not deployed because the current branch is not master ($TRAVIS_BRANCH)."
+  echo "The work is not deployed because the current branch is neither master nor develop ($TRAVIS_BRANCH)."
 fi
