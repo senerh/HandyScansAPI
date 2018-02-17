f5d433aa471bda47c6273f683b915908fd58d994
Hakan Sener <h-akan@hotmail.fr>
2018-02-17 20:01:55 +0100

    test(DAO): add some tests #19

--- src/test/java/util/ConstantUtil.java
+++ src/test/java/util/ConstantUtil.java
@@ -10,5 +10,4 @@ public class ConstantUtil {
     public static final String MANGA_URL = "http://www.lirescan.net/assets/images/mangas/one-piece.jpg";
 
     public static final MangaDTO MANGA_DTO = new MangaDTO(MANGA_SLUG, MANGA_NAME, SCAN, MANGA_URL);
-    public static final ScanDTO SCAN_DTO = new ScanDTO(SCAN);
 }
