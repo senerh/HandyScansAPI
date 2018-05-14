86b2e20c40fa59e208f00f6379b21c0bc0871f13
Hakan Sener <h-akan@hotmail.fr>
2018-05-14 17:30:20 +0200

    conf(travis): disable PROD deployment for pull request
    * update travis config
    
    * update message

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]
+if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-  echo "Starting deployment on production"
-  mv target/shonen-touch-api.war target/shonen-touch-api-3-0-0-SNAPSHOT.war
-  curl -p --insecure "ftp://$DEPLOY_HOST/$DEPLOY_REMOTEDIR/" --user "$DEPLOY_USERNAME:$DEPLOY_PASSWORD" -T "target/shonen-touch-api-3-0-0-SNAPSHOT.war" --ftp-create-dirs
+  echo "Starting PROD deployment..."
+  mv target/shonen-touch-api.war target/shonen-touch-api-3.war
+  curl -p --insecure "ftp://$DEPLOY_HOST/$DEPLOY_REMOTEDIR/" --user "$DEPLOY_USERNAME:$DEPLOY_PASSWORD" -T "target/shonen-touch-api-3.war" --ftp-create-dirs
 elif [ "$TRAVIS_BRANCH" == "develop" ]
 then
-  echo "Starting deployment on integration"
+  echo "Starting DEV deployment..."
   mv target/shonen-touch-api.war target/shonen-touch-api-dev.war
   curl -p --insecure "ftp://$DEPLOY_HOST/$DEPLOY_REMOTEDIR/" --user "$DEPLOY_USERNAME:$DEPLOY_PASSWORD" -T "target/shonen-touch-api-dev.war" --ftp-create-dirs
 else
-  echo "Starting branch deployment ($TRAVIS_BRANCH)"
-  mv target/shonen-touch-api.war target/shonen-touch-api-branch.war
-  curl -p --insecure "ftp://$DEPLOY_HOST/$DEPLOY_REMOTEDIR/" --user "$DEPLOY_USERNAME:$DEPLOY_PASSWORD" -T "target/shonen-touch-api-branch.war" --ftp-create-dirs
+  echo "No deployment for branch $TRAVIS_BRANCH."
 fi
